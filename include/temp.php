a52a7ba3eca5300a3359932828a64b6dfda095d8
zain <zain@aligent.com.au>
2015-03-24 13:37:59 +1030

    temp

--- include/temp.php
+++ include/temp.php
@@ -1,23 +1,4 @@
 <?php
-//return;
-\ZainPrePend\MageInclude\includeMage();
-
-$cron = new Candm_Harmony_Model_Cron();
-$cron->fetchProducts();
-
-die;
-$oProduct = Mage::getModel('catalog/product')->load(5104);
-$cAttribute = Mage::getResourceModel('catalog/product_type_configurable_attribute_collection')
-    ->setProductFilter($oProduct)
-    ->orderByPosition()
-//    ->load()
-;
-\ZainPrePend\lib\T::printr((string) $cAttribute->getSelect());
-//\ZainPrePend\lib\T::printr($cAttribute->toArray());
-/** @var $oAttribute  Mage_Catalog_Model_Resource_Eav_Attribute */
-foreach ($cAttribute as $oAttribute) {
-    \ZainPrePend\lib\T::printr($oAttribute->getProductAttribute()->getData());
-    die;
-}
-
-die;
+//\ZainPrePend\MageInclude\includeMage();
+//echo "<br/>   File:" . __FILE__ . " line:" . __LINE__ . "<br/>\r\n";
+//die;
