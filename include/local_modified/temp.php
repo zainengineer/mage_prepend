b4ae3160155fff5115036c4eb4793437d1c3358f
Zain ul abidin <zain@aligent.com.au>
2017-02-10 10:38:49 +1030

    temp: temp.php is uncommented sections by default

--- include/local_modified/temp.php
+++ include/local_modified/temp.php
@@ -1,9 +1,9 @@
 <?php
 ZainPrePend\MageInclude\sendResourceNotFound();
-//if (!isset($_GET['op']) || ($_GET['op'] != 'temp')){
-//    return;
-//}
-//\ZainPrePend\MageInclude\includeMage();
-//
-//\ZainPrePend\lib\T::printr(1,true,'');
-//die;
+if (!isset($_GET['op']) || ($_GET['op'] != 'temp')){
+    return;
+}
+\ZainPrePend\MageInclude\includeMage();
+
+\ZainPrePend\lib\T::printr(1,true,'');
+die;
